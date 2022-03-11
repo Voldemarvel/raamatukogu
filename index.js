c4cd1751f97b84fe02c60cfd856717f04153e3d1
Voldemarvel <v.viilubusiness@gmail.com>
2022-03-11 16:39:14 +0200

    games added
    lk 27 real 6 jätkata

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 8080
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./docs/swagger.json')
 
+app.get('/games', (req, res) => {
+    res.send(['witcher 3', 'Cyberpunk 2077'])
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
