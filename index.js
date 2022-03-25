d3c4970597a4884eb7b1102034b78c1ef12e0f0f
Voldemarvel <v.viilubusiness@gmail.com>
2022-03-25 12:46:50 +0200

    index.html books list done
    Bugs: CORS null error,
     /books missing/undone.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const app = require('express')()
+const cors = require('cors')
 const port = 8080
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./docs/swagger.json')
 
-app.get('/games', (req, res) => {
+app.use(cors())
+app.use(express.json())
+
+const books = []
+
+app.get('/books', (req, res) => {
     res.send(['The last Wish', 'Varastatud oranž jalgratas'])
 })
 
@@ -12,3 +18,9 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.listen(port, () => {
     console.log('API up at: http://localhost:8080')
 })
+
+
+
+
+//page 68 of 76
+//need to make books list
